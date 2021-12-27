ef3314bea8291165ef7c8dac1ee4a1e525d2de67
wfrsnk <57827569+wfrsnk@users.noreply.github.com>
2021-12-28 02:23:39 +0300

    third task

--- todo/App.js
+++ todo/App.js
@@ -4,6 +4,7 @@ import uuid from 'react-native-uuid';
 
 import Header from './components/Header';
 import ListItems from './components/ListItems';
+import FilterMenu from './components/FilterMenu';
 import Form from './components/Form';
 
 const App = () => {
@@ -13,6 +14,8 @@ const App = () => {
     {text:'do Smth 2', key:'2', completed: false},
     {text:'do Smth 3', key:'3', completed: false}
   ]);
+
+  const [filterValue, setFilterValue] = useState('all');
 
   const addHandler = (text) => {
     setListOfItems( (list) => {
@@ -36,27 +39,64 @@ const App = () => {
 
   const deleteHandler = (key) => {
     setListOfItems( (list) => {
-      return list.filter(listOfItems => listOfItems.key != key)
+      return list.filter(el => el.key != key)
     })
   }
+
+  const filterList = () => {
+    switch (filterValue) {
+      case 'completed': 
+        return listOfItems.filter(el => el.completed === true);
+     case 'active': 
+        return listOfItems.filter(el => el.completed === false);
+      default: 
+        return listOfItems;
+    }
+  }
+
+  const activeCounter = () => {
+    let count = 0;
+    listOfItems.forEach(el => {
+      if(el.completed === false)
+        count++;
+    })
+    return count;
+  }
+
+  const completedCounter = () => {
+    let count = 0;
+    listOfItems.forEach(el => {
+      if(el.completed === true)
+        count++;
+    })
+    return count;
+  }
+
+  const filteredList = filterList();
+  const countOfActive = activeCounter();
+  const countOfCompleted = completedCounter();
 
   return (
     <View style = {styles.section}>
       <Header/>
     
-      <FlatList data={listOfItems} renderItem={({item}) => (
+      <FlatList data={filteredList} renderItem={({item}) => (
         <ListItems el = {item} 
         deleteHandler = {deleteHandler}
         completedHandler = {completedHandler}
         />
       )}/>
-      <Form addHandler={addHandler}/>
+      <FilterMenu 
+        setFilterValue = {setFilterValue}
+        countOfActive={countOfActive}
+        countOfCompleted={countOfCompleted}/>
+      <Form addHandler={addHandler} />
     </View>
   )
 }
 const styles = StyleSheet.create({
   section: {
-    paddingBottom: 80,
+    paddingBottom: 50,
     flex:1
   }
 })
